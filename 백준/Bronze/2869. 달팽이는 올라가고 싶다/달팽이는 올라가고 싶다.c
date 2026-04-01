35fe3a640463686da415afabd1ee3b2192bd6e66
Seongjun Yang (양성준) <seongjun000313@gmail.com>
2026-04-02 08:32:07 +0900

    Update 달팽이는 올라가고 싶다.c

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.c
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.c
@@ -6,7 +6,7 @@ int main(void)
     int how_many_days;
     if((V-A) % (A-B) != 0)
     {
-        how_many_days = (V-A) / (A-B) + 2;
+        how_many_days = (V-A) / (A-B) + 2; // 나머지가 0이 아니면 하루가 더 필요함
     }
     else how_many_days = (V-A) / (A-B) + 1;
     
