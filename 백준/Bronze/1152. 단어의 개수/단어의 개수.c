6b45311daa3a026ec81687866192cd5891c95e89
Seongjun Yang (양성준) <seongjun000313@gmail.com>
2026-04-03 08:28:42 +0900

    Update 단어의 개수.c

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,3 +1,14 @@
+// str[i+1]가 문자일 때만 count하는 곳에서 포함하지 않는 조건이 있었음 
+/* 지피티가 짜준 더 간결한 코드
+  while(str[i] != '\0')
+    {
+        if(str[i] != ' ' && str[i] != '\n' && (i == 0 || str[i-1] == ' '))
+        {
+            cnt++;
+        }
+        i++;
+    }
+*/
 #include <stdio.h>
 #include <string.h>
 
